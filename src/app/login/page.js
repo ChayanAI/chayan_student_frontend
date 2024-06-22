841f320560e3550fc893d47f5b5f70fc64ea84cf
Amit <amitmalakar887@gmail.com>
2024-06-22 23:28:01 +0530

    Added logo

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -71,11 +71,11 @@ const LoginPage = () => {
       <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-16 w-auto"
+            src="/media/images/chayan_logo.png"
             alt="Your Company"
           />
-          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
         </div>
@@ -135,7 +135,7 @@ const LoginPage = () => {
                     type="checkbox"
                     className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                   />
-                  <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-900">
+                  <label htmlFor="remember-me" className="ml-3 mr-2 block text-sm leading-6 text-gray-900">
                     Remember me
                   </label>
                 </div>
